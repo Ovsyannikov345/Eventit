2629ca48dddf54674a151e7287e83ce80a90227d
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-09-15 13:01:44 +0300

    feat: add database connection

---
+++ Eventit/Eventit/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eventit.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
