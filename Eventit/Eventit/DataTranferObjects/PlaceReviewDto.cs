e0aeae48c4372fa910b01b8979546105d2021cdd
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2023-09-29 13:58:21 +0300

    feat: add place review controller

---
+++ Eventit/Eventit/DataTranferObjects/PlaceReviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Eventit.DataTranferObjects
+{
+    public class PlaceReviewDto
+    {
+        public int OrganizerId { get; set; }
+
+        public int PlaceId { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public int Grade { get; set; }
+    }
+}
---
+++ Eventit/Eventit/DataTranferObjects/PlaceReviewGetDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Eventit.DataTranferObjects
+{
+    public class PlaceReviewGetDto
+    {
+        public int OrganizerId { get; set; }
+
+        public int PlaceId { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public int Grade { get; set; }
+    }
+}
