c9d133669f35f0d6f62e4d0af183f73332b6205a
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-05-08 13:22:36 +0300

    feat: create client project structure

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import "moment/locale/en-gb";
+import AppRouter from "./router/AppRouter";
+
+const App = () => {
+    // TODO set correct values.
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#DDC12C",
+            },
+            text: {
+                main: "#000000",
+            },
+        },
+        typography: {
+            h1: {
+                fontSize: "36px",
+            },
+            h2: {
+                fontSize: "24px",
+            },
+            h3: {
+                fontSize: "16px",
+            },
+        },
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 320,
+                md: 768,
+                lg: 1200,
+                xl: 1536,
+            },
+        },
+    });
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterMoment} adapterLocale="en-gb">
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <AppRouter />
+                </BrowserRouter>
+            </ThemeProvider>
+        </LocalizationProvider>
+    );
+};
+
+export default App;
