f24fc596172b27b123835b07b165018a75aeaa73
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-05-16 01:22:01 +0300

    feat: create routing constants

---
+++ client/src/utils/consts.js
@@ -0,0 +1,21 @@
+export const LOGIN_ROUTE = "/login";
+export const REGISTRATION_ROUTE = "/register";
+
+export const USER_DEFAULT_ROUTE = "/events";
+export const COMPANY_DEFAULT_ROUTE = "/my-events";
+
+export const USER_PROFILE_ROUTE = "/user";
+export const COMPANY_PROFILE_ROUTE = "/company";
+
+export const EVENT_CREATION_ROUTE = "/events/create";
+
+export const EVENTS_ROUTE = "/events";
+export const USER_EVENTS_ROUTE = "/my-events";
+export const USER_EVENT_DETAILS_ROUTE = "/events/:id";
+export const COMPANY_EVENTS_ROUTE = "/my-events";
+export const COMPANY_EVENT_DETAILS_ROUTE = "/my-events/:id";
+
+export const SPECIFIC_COMPANY_PROFILE_ROUTE = "/company/:id";
+export const SPECIFIC_USER_PROFILE_ROUTE = "/user/:id";
+
+export const ITEMS_PER_PAGE = 10;
