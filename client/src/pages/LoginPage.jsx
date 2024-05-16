964551108b885af9dbec536eb64ef86c9fac3280
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-05-16 20:28:14 +0300

    feat: create login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,10 +2,29 @@ import React, { useState } from "react";
 import { TextField, Button, Grid, Typography, Snackbar, Alert } from "@mui/material";
 import { useFormik } from "formik";
 import { login } from "../api/authApi";
+import Logo from "../img/logo.png";
+import LoginBackground from "../img/login_background.png";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const LogoContainer = styled.div`
+    width: 200px;
+    height: 70px;
+    border-radius: 10px;
+    overflow: hidden;
+`;
+
+const StyledLogo = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+`;
 
 const LoginPage = () => {
     const [error, setError] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+
+    const navigate = useNavigate();
 
     const displayError = (message) => {
         setErrorMessage(message);
@@ -61,8 +80,28 @@ const LoginPage = () => {
 
     return (
         <>
-            <Grid container justifyContent="center" alignItems="center" style={{ marginTop: "100px" }}>
-                <Grid container item xs={12} sm={6} md={4} xl={3} gap={2} maxWidth={"480px"}>
+            <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                style={{ height: "100%", backgroundImage: `url(${LoginBackground})`, backgroundSize: "cover" }}
+            >
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    xl={3}
+                    gap={2}
+                    maxWidth={"600px"}
+                    style={{ backgroundColor: "#FFFFFF", borderRadius: "15px", padding: "25px" }}
+                >
+                    <Grid container item xs={12} justifyContent={"center"}>
+                        <LogoContainer>
+                            <StyledLogo src={Logo} alt="Logo" />
+                        </LogoContainer>
+                    </Grid>
                     <Typography variant="h4" width={"100%"} textAlign={"center"}>
                         Выполните вход
                     </Typography>
@@ -109,9 +148,23 @@ const LoginPage = () => {
                             fullWidth
                             variant="contained"
                             color="primary"
-                            style={{ marginTop: "20px" }}
+                            style={{ marginTop: "20px", height: "50px", borderRadius: "15px" }}
                         >
                             Войти
+                        </Button>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="secondary"
+                            style={{
+                                marginTop: "20px",
+                                height: "50px",
+                                borderRadius: "15px",
+                                border: "1px solid #999999",
+                            }}
+                            onClick={() => navigate("/register")}
+                        >
+                            Регистрация
                         </Button>
                     </form>
                 </Grid>
