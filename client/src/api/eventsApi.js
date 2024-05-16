d5dfb3adc8b034caf6bacc5902489fd1d34df37c
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-05-16 09:47:42 +0300

    feat: create events api

---
+++ client/src/api/eventsApi.js
@@ -0,0 +1,104 @@
+import { host } from ".";
+import updateToken from "../utils/updateToken";
+
+const getEvents = async () => {
+    try {
+        const response = await host.get("/Events");
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                return await updateToken(getEvents);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+    }
+};
+
+const postEvent = async (eventData) => {
+    try {
+        const response = await host.post("/Events", eventData);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                return await updateToken(postEvent, eventData);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+    }
+};
+
+const getEvent = async (id) => {
+    try {
+        const response = await host.get("/Events/" + id);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                return await updateToken(getEvent, id);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+    }
+};
+
+const getEventChat = async (eventId) => {
+    try {
+        const response = await host.get(`/Events/${eventId}/chat`);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                return await updateToken(getEventChat, eventId);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+    }
+};
+
+const joinEvent = async (id) => {
+    try {
+        const response = await host.get(`/Events/${id}/join`);
+
+        return response;
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 401) {
+                return await updateToken(joinEvent, id);
+            }
+
+            return error.response;
+        } else if (error.request) {
+            return { data: { error: "Сервис временно недоступен" } };
+        } else {
+            return { data: { error: "Ошибка при создании запроса" } };
+        }
+    }
+};
+
+export { getEvents, postEvent, getEvent, getEventChat, joinEvent };
