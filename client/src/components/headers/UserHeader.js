21f46d0fd6c91a99a89ae3e888861e240c9dd9c9
Ilya Ovsyannikov <ilya.ovsyannikov345@gmail.com>
2024-05-19 16:35:22 +0300

    feat: restyle headers

--- client/src/components/headers/UserHeader.js
+++ client/src/components/headers/UserHeader.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { AppBar, Toolbar, Button, IconButton } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AppBar, Toolbar, Button, IconButton, Grid } from "@mui/material";
 import Logo from "../../img/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import EventIcon from "@mui/icons-material/Event";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SupportIcon from "@mui/icons-material/Support";
 import LogoutIcon from "@mui/icons-material/Logout";
 import styled from "styled-components";
 
-// TODO useTheme();
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#729CDB",
-        },
-    },
-});
-
 const LogoContainer = styled.div`
-    width: 100px;
-    height: 35px;
+    width: 120px;
+    height: 55px;
     border-radius: 10px;
     overflow: hidden;
 `;
@@ -38,9 +30,9 @@ const Navigation = styled.div`
 `;
 
 const Activity = styled.div`
-    width: 15%;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    gap: 10px;
     align-items: center;
 `;
 
@@ -54,33 +46,50 @@ const handleLogout = () => {
 
 const UserHeader = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <LogoContainer>
-                        <StyledLogo src={Logo} alt="Logo" />
-                    </LogoContainer>
-                    <Navigation>
-                        <Button color="inherit">Поиск мероприятия</Button>
-                        <Button color="inherit">Ваши мероприятия</Button>
-                    </Navigation>
-                    <Activity>
-                        <IconButton color="inherit" title="Уведомления">
-                            <NotificationsIcon />
-                        </IconButton>
-                        <IconButton color="inherit" title="Профиль">
-                            <AccountCircleIcon />
-                        </IconButton>
-                        <IconButton color="inherit" title="Техническая поддержка">
-                            <SupportIcon />
-                        </IconButton>
-                        <IconButton color="inherit" title="Выход" onClick={handleLogout}>
-                            <LogoutIcon />
-                        </IconButton>
-                    </Activity>
-                </Toolbar>
-            </AppBar>
-        </ThemeProvider>
+        <AppBar position="fixed" sx={{ flexDirection: "row", justifyContent: "center" }}>
+            <Toolbar sx={{ justifyContent: "space-between", width: "100%", maxWidth: "1300px" }}>
+                <LogoContainer>
+                    <StyledLogo src={Logo} alt="Logo" />
+                </LogoContainer>
+                <Navigation>
+                    <Grid container gap={"10px"} ml={"20px"}>
+                        {/* TODO implement */}
+                        <Button
+                            color="inherit"
+                            startIcon={<SearchIcon />}
+                            sx={{ borderBottom: "1px solid #555", borderRadius: "0px" }}
+                        >
+                            Поиск мероприятия
+                        </Button>
+                        {/* TODO implement */}
+                        <Button
+                            color="inherit"
+                            startIcon={<EventIcon />}
+                            sx={{ borderBottom: "1px solid #555", borderRadius: "0px" }}
+                        >
+                            Ваши мероприятия
+                        </Button>
+                    </Grid>
+                </Navigation>
+                <Activity>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Уведомления" sx={{ fontSize: "30px" }}>
+                        <NotificationsIcon fontSize="20px" />
+                    </IconButton>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Профиль" sx={{ fontSize: "30px" }}>
+                        <AccountCircleIcon fontSize="20px" />
+                    </IconButton>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Техническая поддержка" sx={{ fontSize: "30px" }}>
+                        <SupportIcon fontSize="20px" />
+                    </IconButton>
+                    <IconButton color="inherit" title="Выход" sx={{ fontSize: "35px" }} onClick={handleLogout}>
+                        <LogoutIcon fontSize="20px" />
+                    </IconButton>
+                </Activity>
+            </Toolbar>
+        </AppBar>
     );
 };
 
--- client/src/components/headers/СompanyHeader.js
+++ client/src/components/headers/СompanyHeader.js
@@ -1,87 +1,96 @@
-import React from 'react';
-import { AppBar, Toolbar, Button, IconButton } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Logo from '../../img/logo.png';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import SupportIcon from '@mui/icons-material/Support';
-import LogoutIcon from '@mui/icons-material/Logout';
-import styled from 'styled-components';
-
-// TODO useTheme();
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#729CDB',
-    },
-  },
-});
+import React from "react";
+import { AppBar, Toolbar, Button, IconButton, Grid } from "@mui/material";
+import Logo from "../../img/logo.png";
+import CreateIcon from "@mui/icons-material/AddBox";
+import EventIcon from "@mui/icons-material/Event";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SupportIcon from "@mui/icons-material/Support";
+import LogoutIcon from "@mui/icons-material/Logout";
+import styled from "styled-components";
 
 const LogoContainer = styled.div`
-  width: 100px;
-  height: 35px;
-  border-radius: 10px;
-  overflow: hidden;
+    width: 120px;
+    height: 55px;
+    border-radius: 10px;
+    overflow: hidden;
 `;
 
 const StyledLogo = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
 `;
 
 const Navigation = styled.div`
-  flex: 1;
-  font-size: 1.5rem;
-  display: flex;
-  justify-content: space-evenly;
+    flex: 1;
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: space-evenly;
 `;
 
 const Activity = styled.div`
-  width: 15%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    align-items: center;
 `;
 
 const handleLogout = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('role');
-  localStorage.removeItem('refreshToken');
-  localStorage.removeItem("id");
-  window.location.reload();
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("role");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("id");
+    window.location.reload();
 };
 
 const CompanyHeader = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="fixed">
-        <Toolbar>
-          <LogoContainer>
-            <StyledLogo src={Logo} alt="Logo" />
-          </LogoContainer>
-          <Navigation>
-            <Button color="inherit">Создать мероприятие</Button>
-            <Button color="inherit">Ваши мероприятия</Button>
-          </Navigation>
-          <Activity>
-            <IconButton color="inherit" title="Уведомления">
-              <NotificationsIcon />
-            </IconButton>
-            <IconButton color="inherit" title="Профиль">
-              <AccountCircleIcon />
-            </IconButton>
-            <IconButton color="inherit" title="Техническая поддержка">
-              <SupportIcon />
-            </IconButton>
-            <IconButton color="inherit" title="Выход" onClick={handleLogout}>
-              <LogoutIcon />
-            </IconButton>
-          </Activity>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
-  );
+    return (
+        <AppBar position="fixed" sx={{ flexDirection: "row", justifyContent: "center" }}>
+            <Toolbar sx={{ justifyContent: "space-between", width: "100%", maxWidth: "1300px" }}>
+                <LogoContainer>
+                    <StyledLogo src={Logo} alt="Logo" />
+                </LogoContainer>
+                <Navigation>
+                    <Grid container gap={"10px"} ml={"20px"}>
+                        {/* TODO implement */}
+                        <Button
+                            color="inherit"
+                            startIcon={<CreateIcon />}
+                            sx={{ borderBottom: "1px solid #555", borderRadius: "0px" }}
+                        >
+                            Создать мероприятие
+                        </Button>
+                        {/* TODO implement */}
+                        <Button
+                            color="inherit"
+                            startIcon={<EventIcon />}
+                            sx={{ borderBottom: "1px solid #555", borderRadius: "0px" }}
+                        >
+                            Ваши мероприятия
+                        </Button>
+                    </Grid>
+                </Navigation>
+                <Activity>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Уведомления" sx={{ fontSize: "30px" }}>
+                        <NotificationsIcon fontSize="20px" />
+                    </IconButton>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Профиль" sx={{ fontSize: "30px" }}>
+                        <AccountCircleIcon fontSize="20px" />
+                    </IconButton>
+                    {/* TODO implement */}
+                    <IconButton color="inherit" title="Техническая поддержка" sx={{ fontSize: "30px" }}>
+                        <SupportIcon fontSize="20px" />
+                    </IconButton>
+                    <IconButton color="inherit" title="Выход" sx={{ fontSize: "30px" }} onClick={handleLogout}>
+                        <LogoutIcon fontSize="20px" />
+                    </IconButton>
+                </Activity>
+            </Toolbar>
+        </AppBar>
+    );
 };
 
 export default CompanyHeader;
